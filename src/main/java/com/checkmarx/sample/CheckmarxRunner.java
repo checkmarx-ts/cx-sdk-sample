69090d3abb015159cc0cb0d1ccf321734d3f36d3
Ken McDonald <ken.mcdonald@checkmarx.com>
2019-12-18 16:22:14 -0500

    Update SDK version and complete sample code

--- src/main/java/com/checkmarx/sample/CheckmarxRunner.java
+++ src/main/java/com/checkmarx/sample/CheckmarxRunner.java
@@ -1,16 +1,32 @@
 package com.checkmarx.sample;
 
+import com.checkmarx.sdk.dto.cx.CxTeam;
+import com.checkmarx.sdk.exception.CheckmarxException;
+import com.checkmarx.sdk.service.CxClient;
 import org.slf4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class CheckmarxRunner implements CommandLineRunner {
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(CheckmarxRunner.class);
 
+    private final CxClient client;
+
+    public CheckmarxRunner(CxClient client) {
+        this.client = client;
+    }
+
     @Override
     public void run(String[] args) {
-        log.info("test");
+        try {
+            List<CxTeam> teams = client.getTeams();
+            teams.forEach(t -> log.info(t.getFullName()));
+        } catch (CheckmarxException e) {
+            log.error("Error occurred while getting teams");
+        }
     }
 }
 
