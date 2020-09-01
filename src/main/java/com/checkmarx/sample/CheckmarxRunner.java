e38657ff608bd36b7733a4a718e7ae946a20de37
Ken McDonald <ken.mcdonald@checkmarx.com>
2020-09-01 14:08:03 -0400

    Additional sample logic

--- src/main/java/com/checkmarx/sample/CheckmarxRunner.java
+++ src/main/java/com/checkmarx/sample/CheckmarxRunner.java
@@ -1,5 +1,6 @@
 package com.checkmarx.sample;
 
+import com.checkmarx.sdk.config.CxProperties;
 import com.checkmarx.sdk.dto.cx.CxTeam;
 import com.checkmarx.sdk.exception.CheckmarxException;
 import com.checkmarx.sdk.service.CxClient;
@@ -14,15 +15,27 @@ public class CheckmarxRunner implements CommandLineRunner {
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(CheckmarxRunner.class);
 
     private final CxClient client;
+    private final CxProperties properties;
 
-    public CheckmarxRunner(CxClient client) {
+    public CheckmarxRunner(CxClient client, CxProperties properties) {
         this.client = client;
+        this.properties = properties;
     }
 
     @Override
     public void run(String[] args) {
         try {
+            //list out teams
             List<CxTeam> teams = client.getTeams();
+            teams.forEach(t -> log.info(t.getFullName()));
+            //get the base team from the property file
+            String teamId = client.getTeamId(properties.getTeam());
+            //create a new team under this above team
+            String newTeamId = client.createTeam(teamId, "MyNewTeam");
+            //log the new team id
+            log.info("New team id: {}", newTeamId);
+            //list out teams again
+            teams = client.getTeams();
             teams.forEach(t -> log.info(t.getFullName()));
         } catch (CheckmarxException e) {
             log.error("Error occurred while getting teams");
